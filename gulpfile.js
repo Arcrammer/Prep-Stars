f065298d95f24fd4e59b5f28427037e3ab020487
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-04-12 08:24:35 -0400

    Initial Commit; Begin conversion to Keystone

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,53 @@
 var gulp = require('gulp');
-var exec = require('child_process').exec;
+var jshint = require('gulp-jshint');
+var jshintReporter = require('jshint-stylish');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell')
 
-gulp.task('default', function () {
-  // Serve it
-  exec('nodemon');
+var sass = require('gulp-sass');
 
-  // Watch those assets
-  exec('compass watch public');
 
-  // Run Browser-Sync
-  exec('browser-sync start --config="bs-config.js"')
+var paths = {
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+
+,
+	'style': {
+		all: './public/styles/**/*.scss',
+		output: './public/styles/'
+	}
+
+};
+
+// gulp lint
+gulp.task('lint', function(){
+	gulp.src(paths.src)
+		.pipe(jshint())
+		.pipe(jshint.reporter(jshintReporter));
 });
+
+// gulp watcher for lint
+gulp.task('watch:lint', function () {
+	gulp.watch(paths.src, ['lint']);
+});
+
+
+gulp.task('watch:sass', function () {
+	gulp.watch(paths.style.all, ['sass']);
+});
+
+gulp.task('sass', function(){
+	gulp.src(paths.style.all)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(paths.style.output));
+});
+
+
+gulp.task('runKeystone', shell.task('node keystone.js'));
+gulp.task('watch', [
+
+  'watch:sass',
+
+  'watch:lint'
+]);
+
+gulp.task('default', ['watch', 'runKeystone']);
