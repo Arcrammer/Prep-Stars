44f7f9baeec509beb61a310a3da17c00c8e9c246
Alexander Rhett Crammer <alexander2475914@gmail.com>
2016-04-20 09:13:14 -0400

    Fix navigation link bugs
    Refactor a bit more

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
+  // Slick Slideshow
   $('.slideshow').slick({
     dots: true,
     autoplay: true
+  });
+
+  // Dropdown Menu
+  $('.dropbtn, .dropdown-content').hover(function () {
+    $('.dropdown-content').css('display', 'inline');
+  }, function () {
+    $('.dropdown-content').css('display', 'none');
   });
 });
